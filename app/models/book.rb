077b17fa144f617795868f918dd0a05175fdd50f
Trevor Frese <trevor.frese@gmail.com>
2012-05-21 21:15:28 -0700

    Added a book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :owner_username, :title, :isbn, :image_address
+  attr_accessible :author, :owner, :title, :isbn, :image_address
+
+validates :isbn, presence: true, length: {maximum:17}
+
+validates_presence_of :owner, :title, :author, :image_address
+
+
+
 end
