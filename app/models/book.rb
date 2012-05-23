d9525b60ee5d13e7bf8539f8a6e1e391f9b9ce07
Ubuntu <ubuntu@ip-10-204-43-245.ec2.internal>
2012-05-23 15:30:39 +0000

    changed gemfile so it works

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :owner, :title, :isbn, :image_address
+  attr_accessible :author, :title, :isbn, :image_address #, :owner
 
 validates :isbn, presence: true, length: {maximum:17}
 
-validates_presence_of :owner, :title, :author, :image_address
+validates_presence_of :title, :author, :image_address #,:owner
 
 
 
