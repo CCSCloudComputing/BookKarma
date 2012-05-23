cbcb4a9b6fa14d2873c1c3e3ec5472257e70b813
kwojcik <kdwojcik@gmail.com>
2012-05-23 16:00:51 +0000

    bug fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :title, :isbn, :image_address #, :owner
+  attr_accessible :author, :title, :isbn, :image_address, :owner
 
 validates :isbn, presence: true, length: {maximum:17}
 
-validates_presence_of :title, :author, :image_address #,:owner
+validates_presence_of :title, :author, :image_address,:owner
 
 
 
