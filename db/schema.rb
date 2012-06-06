0b7a14123a64c265aeed71fd937c4a0853443c40
Shayon Javadizadeh <shyjavadi@yahoo.com>
2012-06-06 02:36:22 -0700

    Added a Nav bar and fixed spacing issues from my last edit.  Also made the links take you to the correct address.

--- db/schema.rb
+++ db/schema.rb
@@ -38,11 +38,11 @@ ActiveRecord::Schema.define(:version => 20120605042207) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password_digest"
-    t.string   "auth_token"
     t.integer  "points"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
