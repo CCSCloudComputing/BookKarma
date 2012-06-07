f8c1be1a10bb21aa371163916f321e2652fa83aa
Tai Rodrig <taicapra@gmail.com>
2012-06-06 21:34:14 -0700

    Stores the images locally. you MUST create a images folder in the root of your application.

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(:version => 20120607032557) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password_digest"
-    t.string   "auth_token"
     t.integer  "points"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
